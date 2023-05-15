38b7b82e5aa7a4f548ca5c46f18d81113b629c97
Julius Kayne <juliuskayne07@gmail.com>
2023-05-16 05:11:34 +0700

    Try: check base url

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -4,6 +4,7 @@ class Home extends Controller{
     
     public function index()
     {
+        var_dump(dirname(__FILE__));
         $user = $this->model('user_model')->getUser();
         $this->view('home/index');
     }
