a59d8f949d15f53f47f85c2a9cef3c67882c8094
Julius Kayne <juliuskayne07@gmail.com>
2023-05-16 05:38:53 +0700

    try base url

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <h5 class="list-style-none">Coba</h5>
-    <img src="./assets/img/aki.png" alt="">
-    <a href="<?= BASE_URL; ?>dashboard">Klik sini</a>
+    <img src="../../../public_html/assets/img/aki.png" alt="">
+    
 </body>
 </html>
