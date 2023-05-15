69977fcc1429936548c7730238b6f25096bbe40d
Julius Kayne <juliuskayne07@gmail.com>
2023-05-16 05:57:04 +0700

    try base url

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -4,7 +4,6 @@ class Home extends Controller{
     
     public function index()
     {
-        var_dump(BASE_URL."assets/css/general.css");
         $user = $this->model('user_model')->getUser();
         $this->view('home/index');
     }
