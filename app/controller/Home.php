205191294c97874eed4128735ac20f90a80f5b2e
Julius Kayne <juliuskayne07@gmail.com>
2023-05-16 05:03:25 +0700

    Try: check the server

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -4,6 +4,7 @@ class Home extends Controller{
     
     public function index()
     {
+        echo json_encode($_SERVER);
         $user = $this->model('user_model')->getUser();
         $this->view('home/index');
     }
