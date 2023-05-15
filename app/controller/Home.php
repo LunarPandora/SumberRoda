130a61fd64064ffeed80fe184690589b58d7b851
Julius Kayne <juliuskayne07@gmail.com>
2023-05-16 05:09:20 +0700

    Test: Base url

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -4,7 +4,6 @@ class Home extends Controller{
     
     public function index()
     {
-        echo json_encode($_SERVER);
         $user = $this->model('user_model')->getUser();
         $this->view('home/index');
     }
