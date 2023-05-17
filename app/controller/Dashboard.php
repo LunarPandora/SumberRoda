dd3c4f33d1a12005fd1168f44f289539ccb0eb05
Julius Kayne <juliuskayne07@gmail.com>
2023-05-17 10:47:25 +0700

    Try without database, and bsae url

--- app/controller/Dashboard.php
+++ app/controller/Dashboard.php
@@ -2,15 +2,11 @@
 
 class Dashboard extends Controller{
     public function index(){
-<<<<<<< HEAD
         $data=[ 
             "nama" => "suhendri",
             "kelas" => "STI-B"
         ];
         $this->view('dashboard/index', $data) ;
         
-=======
-        $this->view('dashboard/index');
->>>>>>> 0ae2ff666e0fb691345615d8728004ad68acef03
     }
 }
--- app/controller/Home.php
+++ app/controller/Home.php
@@ -4,8 +4,8 @@ class Home extends Controller{
     
     public function index()
     {
-        $user = $this->model('user_model')->getUser();
-        var_dump($user['nama']);
+        // $user = $this->model('user_model')->getUser();
+        // var_dump($user['nama']);
         $this->view('home/index');
     }
 }
