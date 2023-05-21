dbf898bde11ba4214c6c664d372a91d8cc89f37a
Julius Kayne <juliuskayne07@gmail.com>
2023-05-21 08:11:19 +0700

    Try base uri

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -4,9 +4,9 @@ class Home extends Controller{
     
     public function index()
     {
-        echo BASE_URL;
         // $user = $this->model('user_model')->getUser();
         // var_dump($user['nama']);
+        echo BASE_URL;
         $this->view('home/index');
     }
 }
