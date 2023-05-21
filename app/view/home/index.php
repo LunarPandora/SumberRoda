1a25eef196d7dc81e30997bd9dd5aeac96c11111
Julius Kayne <juliuskayne07@gmail.com>
2023-05-21 08:20:24 +0700

    try the asset

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style lang="css" src="<?= BASE_URL ?>assets/css/style.css"></style>
 </head>
 <body>
     <h5 class="list-style-none">Coba</h5>
-    <a href="<?= BASE_URL ?>dashboard">Klik sini</a>
+    <a class="btn" href="<?= BASE_URL ?>dashboard">Klik sini</a>
 </body>
 </html>
