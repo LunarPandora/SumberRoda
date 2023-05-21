0ee4a40506b0b624108b3862b618a812e20ddb4a
Julius Kayne <juliuskayne07@gmail.com>
2023-05-21 08:15:15 +0700

    Change Base url

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -4,8 +4,9 @@ class Home extends Controller{
     
     public function index()
     {
-        $user = $this->model('user_model')->getUser();
-        var_dump($user['nama']);
+        echo BASE_URL;
+        // $user = $this->model('user_model')->getUser();
+        // var_dump($user['nama']);
         $this->view('home/index');
     }
 }
