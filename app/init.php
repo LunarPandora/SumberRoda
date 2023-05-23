d41c56694302e7512066645334f97e24a69f7bdf
Suhendri <suhendri@itbss.ac.id>
2023-05-23 22:04:00 +0700

    Try: connect

--- app/init.php
+++ app/init.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 // require "config/Config.php";
 // require "config/DatabaseAccount.php";
-=======
-require "config/Config.php";
-require "config/DatabaseAccount.php";
-require "config/Auth.php";
->>>>>>> 6f1801b37ab064c8f42d386e707d350ea143cfc5
 
 require "core/App.php";
 // require "core/Database.php";
