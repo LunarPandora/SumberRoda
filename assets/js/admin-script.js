823b93ce23d9ed078aaf37bd5e24a6db967dca02
Julius Kayne <juliuskayne07@gmail.com>
2023-05-31 05:46:27 +0700

    Finisihing CRUD Merek produk, strukur CRUD

--- assets/js/admin-script.js
+++ assets/js/admin-script.js
@@ -1,19 +1,22 @@
-const unsetAction = (url) => {
-    let isSuccess = false;
+const getData = (url, formID) => {
+    const data = new FormData($(`#${formID}`)[0]);
     
     $.ajax({
         type: 'POST',
         url: url,
+        data: data,
         contentType: false,
         cache: false,
         processData: false
     })
     .done((response) => {
-        
+        console.log(response);
     })
     .fail((err) => {
-        
+        console.log(err)
     })
-    
-    return isSuccess;
+}
+
+function closeModal (url) {
+    location.href = url;
 }
